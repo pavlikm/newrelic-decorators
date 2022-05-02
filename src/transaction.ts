0991a473bb02127411b072200d3c379592db9492
pavlik <michal.pavlik@ringieraxelspringer.sk>
2022-05-02 16:10:35 +0200

    fix default parameter

--- src/transaction.ts
+++ src/transaction.ts
@@ -1,4 +1,4 @@
-export const NewRelicTransaction = (transactionName: string) => (
+export const NewRelicTransaction = (transactionName: string = "") => (
     target: any,
     name: string,
     descriptor: PropertyDescriptor
